9f71d76398aaac67d08ad63b5fd4cc5f81b74b1c
islombek202 <islombekkarimov246@gmail.com>
2026-05-19 15:36:46 +0500

    Refactor message handling to convert text between Cyrillic and Latin alphabets

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from pydoc import text
 import telebot
 import os
 from dotenv import load_dotenv
+from transliterate import to_cyrillic, to_latin
 load_dotenv()
-TOKEN="8780903331:AAEC1X6nkjCWzFgm2GZc27XEaHY5q37SbsU"
+
 bot = telebot.TeleBot(TOKEN, parse_mode=None) 
 TOKEN=os.getenv("BOT_TOKEN")
 
@@ -16,6 +18,11 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
- bot.reply_to(message, message.text)
 
+
+
+    if text.isascii():
+        bot.reply_to(message, to_cyrillic(text))
+    else:
+        bot.reply_to(message, to_latin(text))
 bot.infinity_polling()
